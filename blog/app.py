b3a14a8bd43d394c66c72373a03db22d5fec450d
rifkegribenes <rifkegribenes@gmail.com>
2019-04-12 22:44:59 -0700

    more patching

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,14 @@
 MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit.'
+POST_TEMPLATE = '''
+--- {} ---
+
+{}
+
+'''
 
 blogs = dict()
+from blog import Blog
+from post import Post
 
 
 def menu():
@@ -11,8 +19,44 @@ def menu():
 
     print_blogs()
     selection = input(MENU_PROMPT)
-
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
         print('• {}'.format(blog))
+
+def ask_create_blog():
+    title = input('What is the title of the blog? ')
+    author = input('What is your name? ')
+    blogs[title] = Blog(title, author)
+
+def ask_read_blog():
+    title = input('Which blog do you want to read? ')
+
+    print_posts(blogs[title])
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_post(post)
+
+def print_post(post):
+    print(POST_TEMPLATE.format(post.title, post.content))
+
+
+def ask_create_post():
+    blog_title = input('Which blog do you want to post to? ')
+    title = input('What is the title of the post? ')
+    content = input('What is the content of the post? ')
+    new_post = Post(title, content)
+    for blog in blogs:
+        if blog['title'] == title:
+            blog.posts.append(new_post)
--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
+from post import Post
 
 
 class AppTest(TestCase):
@@ -16,9 +17,6 @@ class AppTest(TestCase):
                 app.menu()
                 mocked_print_blogs.assert_called()
 
-
-
-
     def test_print_blogs(self):
         blog = Blog('Test', 'Test Author')
         app.blogs = {'Test': blog}
@@ -27,7 +25,45 @@ class AppTest(TestCase):
             app.print_blogs()
             mocked_print.assert_called_with('• Test by Test Author (0 posts)')
 
+    def test_ask_create_blogs(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
 
+        self.assertIsNotNone(app.blogs.get('Test'))
+
+    def test_ask_read_blog(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+
+                mocked_print_posts.assert_called_with(blog)
+
+    def test_print_posts(self):
+        blog = Blog('Test', 'Test Author')
+        blog.create_post('Test Post', 'Test Content')
+        app.blogs = {'Test': blog}
+
+        with patch('app.print_post') as mocked_print_post:
+            app.print_posts(blog)
+
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+    def test_print_post(self):
+        post = Post('Post title', 'Post content')
+        expected_print = '''
+--- Post title ---
+
+Post content
+
+'''
+        with patch('builtins.print') as mocked_print:
+            app.print_post(post)
+
+            mocked_print.assert_called_with(expected_print)
 
 
 
